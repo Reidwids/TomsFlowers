a65661c70d81642f323524a67df709f9ef1184ae
Reidwids <reidwids@gmail.com>
2022-09-11 14:47:14 -0400

    Adding theme and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,29 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Base from "./components/Base/Base";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#8d5a64",
-		},
-		secondary: {
-			main: "#387668",
-		},
-	},
+export const theme = extendTheme({
+  fonts: {
+    body: "Nunito Sans, sans-serif",
+  },
+  colors: {
+    flowerPink: "#8d5a64",
+    flowerGreen: "#387668",
+  },
 });
 
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Router>
-				<Routes>
-					<Route path="/*" element={<Base />} />
-				</Routes>
-			</Router>
-		</ThemeProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<Base />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;
