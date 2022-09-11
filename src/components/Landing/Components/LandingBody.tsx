9d112e981ae070afcf37380141ba769ab742ae5f
Reidwids <reidwids@gmail.com>
2022-09-11 14:47:30 -0400

    Adding mobile nav

--- src/components/Landing/Components/LandingBody.tsx
+++ src/components/Landing/Components/LandingBody.tsx
@@ -1,39 +1,139 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Collapse,
+  Flex,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { motion } from "framer-motion";
+import { theme } from "../../../App";
 type PropTypes = {
   isDesktop: boolean;
 };
 
 export default function LandingBody({ isDesktop }: PropTypes) {
+  const navigate = useNavigate();
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
-    <Box
-      sx={{
-        height: "100vh",
-        backgroundColor: "lightblue",
-        width: "100%",
-        zIndex: "0",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Box sx={{ maxWidth: "1200px", width: "100%" }}>
-        <Box
-          sx={{
-            display: "flex",
-            height: isDesktop ? "100px" : "60px",
-            justifyContent: "space-between",
-            alignItems: "center",
-            margin: "0 30px",
+    <Flex h="100vh" bgColor="lightblue" w="100%" justify={"center"}>
+      <Box maxW={"1200px"} w="100%" color="flowerPink">
+        <motion.div
+          style={{ width: "100%" }}
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              delay: 3.5,
+              duration: 3,
+              ease: [0.87, 0, 0.13, 1],
+            },
           }}
         >
-          <Typography color="primary" sx={{ fontSize: "18px" }}>
-            Location & Hours
-          </Typography>
-          <Typography sx={{ fontSize: "18px" }} color="primary">
-            About
-          </Typography>
-        </Box>
+          {isDesktop ? (
+            <Flex
+              h={isDesktop ? "100px" : "60px"}
+              justify="space-between"
+              align="center"
+              m="0 30px"
+            >
+              <Text
+                _hover={{
+                  fontWeight: "600",
+                }}
+                cursor="pointer"
+                fontSize="18px"
+                position="relative"
+                zIndex={3}
+                onClick={() => navigate("#About")}
+              >
+                About
+              </Text>
+              <Text
+                _hover={{
+                  fontWeight: "600",
+                }}
+                cursor="pointer"
+                fontSize="18px"
+                position="relative"
+                zIndex={3}
+                onClick={() => navigate("#LocationHours")}
+              >
+                Location & Hours
+              </Text>
+            </Flex>
+          ) : (
+            <>
+              <Flex
+                h={isDesktop ? "100px" : "60px"}
+                justify="space-between"
+                align="center"
+                m="0 30px"
+                zIndex={3}
+              >
+                <IconButton
+                  onClick={onToggle}
+                  cursor={"pointer"}
+                  color="flowerPink"
+                  zIndex={3}
+                  icon={
+                    isOpen ? (
+                      <CloseIcon w={3} h={3} />
+                    ) : (
+                      <HamburgerIcon w={5} h={5} />
+                    )
+                  }
+                  variant={"ghost"}
+                  aria-label={"Toggle Navigation"}
+                />
+              </Flex>
+              <Collapse in={isOpen} animateOpacity>
+                <Stack
+                  p={"10px 30px"}
+                  display={isDesktop ? "none" : "block"}
+                  bg="white"
+                >
+                  <Stack spacing={4} onClick={onToggle}>
+                    <Flex
+                      py={2}
+                      zIndex={3}
+                      as={Link}
+                      href={""}
+                      justify={"space-between"}
+                      align={"center"}
+                      _hover={{
+                        textDecoration: "none",
+                      }}
+                    >
+                      <Text fontWeight={600}>About</Text>
+                    </Flex>
+                    <hr color={theme.colors.flowerPink} />
+                    <Flex
+                      py={2}
+                      zIndex={3}
+                      as={Link}
+                      href={""}
+                      justify={"space-between"}
+                      align={"center"}
+                      _hover={{
+                        textDecoration: "none",
+                      }}
+                    >
+                      <Text fontWeight={600}>Location & Hours</Text>
+                    </Flex>
+                  </Stack>
+                </Stack>
+              </Collapse>
+            </>
+          )}
+        </motion.div>
       </Box>
-    </Box>
+    </Flex>
   );
 }
--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,51 +1,18 @@
 import React from "react";
-import Rose from "../../assets/icons/Rose.svg";
-import TomsFlowersNoRose from "../../assets/images/TomsFlowersNoRose.svg";
-import { Box, useMediaQuery } from "@mui/material";
-import { motion } from "framer-motion";
 import LandingAnimation from "./Components/LandingAnimation";
 import LandingBody from "./Components/LandingBody";
+import { useMediaQuery } from "@chakra-ui/react";
 
 export default function Landing() {
-	// Variables //
-	const isDesktop = useMediaQuery("(min-width:600px)");
-	const whiteBox = {
-		initial: {
-			opacity: 1,
-		},
-		animate: {
-			opacity: 0,
-			transition: {
-				delay: 3.5,
-				duration: 3,
-				ease: [0.87, 0, 0.13, 1],
-			},
-		},
-	};
-	console.log(isDesktop);
-	const logoTransition = {
-		initial: {
-			height: "100vh",
-		},
-		animate: {
-			height: isDesktop ? "100px" : "60px",
-			transition: {
-				delay: 3,
-				duration: 2,
-				ease: "easeInOut",
-			},
-		},
-	};
-	// Functions //
+  // Variables //
+  const [isDesktop] = useMediaQuery("(min-width:720px)");
 
-	// Use Effects //
-
-	// Render //
-	return (
-		<>
-			{/* Page Container */}
-			<LandingBody isDesktop={isDesktop} />
-			<LandingAnimation isDesktop={isDesktop} />
-		</>
-	);
+  // Render //
+  return (
+    <>
+      {/* Page Container */}
+      <LandingBody isDesktop={isDesktop} />
+      <LandingAnimation isDesktop={isDesktop} />
+    </>
+  );
 }
